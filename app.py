0ea5024add3e67040899edd6816a615e41fd193e
Farias1douglas <douglas.v@escolar.ifrn.edu.br>
2025-06-25 20:34:46 +0000

    0.10

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 caminho_db = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.getenv('DB_PATH'))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{caminho_db}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_password}@{db_host}/{db_mydb}"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
